87768f0bfc646de21a37dd54b6b02e78a3cc29ae
Egozi <saar.egozi@here.com>
2017-11-12 14:28:27 +0200

    Working non-responsive server, Displayed date and hour are wrong

--- server/server.go
+++ server/server.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+	"html/template"
+	"log"
 )
 
-func HandleFunction(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Yosi")
+type HomePageStruct struct{
+	Date string
+	Time string
+}
+
+
+func HomePageFunction(w http.ResponseWriter, r *http.Request){
+	now := time.Now()
+	homePage := HomePageStruct{
+		Date:now.Format("01-01-1990"),
+		Time:now.Format("01:01:01"),
+	}
+	log.Println(now)
+
+	t,err := template.ParseFiles("/Users/egozi/GoglandProjects/own-server/html/homePage.html")
+	if err != nil {
+		log.Println("template parsing error: ", err)
+	}
+
+	err = t.Execute(w,homePage)
+	if err != nil {
+		log.Println("executaion error: ", err)
+	}
+
 }
 
 func main() {
-	http.HandleFunc("/",HandleFunction)
+	http.HandleFunc("/",HomePageFunction)
 	http.ListenAndServe(":50001",nil)
 }
